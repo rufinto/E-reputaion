446f42da7494229edc10023d4ca0d6f777899230
Le Roy Paul <paul.le-roy@student-cs.fr>
2024-06-03 15:00:18 +0200

    doc

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import nltk
 
 # Importing the dataset
 def afficher_dataset():
-    chemin = "MIGRTWIT20112022.csv"
-    dataset = pd.read_csv(chemin, sep='\t')
+    chemin = 'FR-L-MIGR-TWIT-2011-2022.csv'
+    dataset = pd.read_csv(chemin, sep=';')
     print(dataset.head())
 afficher_dataset()
