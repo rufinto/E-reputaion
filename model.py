ddd9e3ec01941e09b43702f72054cbafa2221f52
Toukap Nguepteo Joan-Rufin <joan-rufin.toukap-nguepteo@student-cs.fr>
2024-06-03 14:42:27 +0200

    m

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 dataset_name = "FR-L-MIGR-TWIT-2011-2022.csv"
-dataset = pd.read_csv(dataset_name)
+dataset = pd.read_csv("FR-L-MIGR-TWIT-2011-2022.csv")
 colonne = ["data__id", "data__text", "data__created_at", "author__username"]
 
 print(dataset[colonne[0]])
