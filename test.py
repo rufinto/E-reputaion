b5508e01d3b879e17ebb8564f34eb7efb83d6a21
Le Roy Paul <paul.le-roy@student-cs.fr>
2024-06-03 14:51:27 +0200

    mise à jour

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import nltk 
+
+# Importing the dataset
+def afficher_dataset():
+    chemin = "MIGRTWIT20112022.csv"
+    dataset = pd.read_csv(chemin, sep='\t')
+    print(dataset.head())
+afficher_dataset()
