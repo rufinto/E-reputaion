06d7818177428d6bf2f32467194e60e76f4e9ac9
Le Roy Paul <paul.le-roy@student-cs.fr>
2024-06-04 09:20:20 +0200

    4/06

--- test.py
+++ test.py
@@ -2,15 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import nltk 
-nltk.download('vader_lexicon')
+import flair
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 
-sid = SentimentIntensityAnalyzer()
 
 # Importing the dataset
-def afficher_dataset():
-    chemin = 'FR-L-MIGR-TWIT-2011-2022.csv'
-    dataset = pd.read_csv(chemin, sep=';')
-    print(dataset.head())
-afficher_dataset()
+chemin = 'FR-L-MIGR-TWIT-2011-2022.csv'
+dataset = pd.read_csv(chemin, sep=';')
+
+
+
+#for i in dataset['data__text'].head():
+    #print(word_tokenize(i))
+
+french_stopwords = set(stopwords.words('french'))
+stemmer = nltk.stem.SnowballStemmer('french')
+print(french_stopwords)
+for i in dataset['data__text'].head():
+    print([stemmer.stem(word) for word in word_tokenize(i) if word.lower() not in french_stopwords])
+    #print(word_tokenize(i))
+    #print([word for word in word_tokenize(i) if word.lower() not in french_stopwords])
