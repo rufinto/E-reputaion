a9da4a012939f9f78ea0acb1e9a17a544819c3ce
Toukap Nguepteo Joan-Rufin <joan-rufin.toukap-nguepteo@student-cs.fr>
2024-06-04 09:22:11 +0200

    update

--- model.py
+++ model.py
@@ -4,8 +4,11 @@ import sklearn
 import nltk
 nltk.download('punkt')
 nltk.download("stopwords")
+nltk.download('wordnet')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk import FreqDist
 
 dataset_name = "FR_L_MIGR_TWIT_2011_2022.csv"
 dataset = pd.read_csv(dataset_name, sep=";")
@@ -24,24 +27,31 @@ for tweet in dataset[colums[1]]:
 #Tokenization
 
 sac_de_mots = word_tokenize(corpus)
-#print(len(sac_de_mots))
 
 #filtration
 
 stop_words = set(stopwords.words("french"))
 mots_utiles = {"ne", "pas", "n"} # stop word à conserver
-mots_inutiles = {"\\", "\\n", "[", "]", ":", ",", "#", "@", "»", "«", "''", ".", "https", "/"} #stop word supplémentaires
+mots_inutiles = {"\\", "\\n", "[", "]", "(", ")", "-", ":", ",", "#", "@", "»", "«", "''", "’", "'", ".", "https", "http", "/", "``","&"} #stop word supplémentaires
 
 stop_words = stop_words.difference(mots_utiles)
 stop_words = stop_words.union(mots_inutiles)
 
-print(stop_words)
-filtered_sac_de_mot = [word for word in sac_de_mots if word.casefold() not in stop_words]
+filtered_sac_de_mots = [word for word in sac_de_mots if word.casefold() not in stop_words]
 
 def retire_site_web(liste):
     for word in liste:
         if word[0:2] == "//":
             liste.remove(word)
 
-retire_site_web(filtered_sac_de_mot)
-print(filtered_sac_de_mot, len(filtered_sac_de_mot))
+retire_site_web(filtered_sac_de_mots)
+#print(filtered_sac_de_mot, len(filtered_sac_de_mot))
+
+#lemmatisation
+lemmatizer = WordNetLemmatizer()
+lemmatized_sac_de_mots = [lemmatizer.lemmatize(word) for word in filtered_sac_de_mots]
+
+#calcul tf
+
+tf = FreqDist(lemmatized_sac_de_mots)
+print(tf.most_common(50), len(tf))
