31cdf25b1d3ec2fbc205371a5c792d2b12a52b4d
Le Roy Paul <paul.le-roy@student-cs.fr>
2024-06-04 15:36:22 +0200

    chunking

--- labelling.py
+++ labelling.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import nltk
 from nltk.tag import StanfordPOSTagger
+import stanza 
 import test
+import re
+
+#nlp = stanza.Pipeline('fr', processors='tokenize, pos, lemma, depparse')
 
 jar = 'C:/Users/paull/Documents/EI DataWeb/stanford-postagger-full-2020-11-17/stanford-postagger-4.2.0.jar'
 model = 'C:/Users/paull/Documents/EI DataWeb/stanford-postagger-full-2020-11-17/models/french-ud.tagger'
@@ -13,10 +17,40 @@ chemin = 'FR-L-MIGR-TWIT-2011-2022.csv'
 dataset = pd.read_csv(chemin, sep=';')
 
 pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8')
-res = pos_tagger.tag(test.filtered_tweets[100])
-print (res)
+res = pos_tagger.tag(test.filtered_tweets[90])
+#print (res)
 
-grammar = "NP: {<DT>?<ADJ>*<VERB>?<NOUN>}"
+
+#grammar = "NP: {<DT>?<ADJ>*<VERB>?<NOUN>}"
+
+grammar = r"""
+  NP: {<DET>?<ADJ>*<NOUN>+}         # Groupe nominal: déterminant optionnel, adjectifs optionnels, un ou plusieurs noms
+  VP: {<VERB>+<NP|PP|CLAUSE>+}      # Groupe verbal: un ou plusieurs verbes suivis de groupes nominaux, prépositionnels ou de clauses
+  PP: {<ADP><NP>}                    # Groupe prépositionnel: préposition suivie d'un groupe nominal
+  ADJP: {<ADJ>+<CC>?<ADJ>*}          # Groupe adjectival: un ou plusieurs adjectifs, optionnellement coordonnés par une conjonction de coordination
+  CLAUSE: {<NP><VP>}                 # Clause: groupe nominal suivi d'un groupe verbal
+"""
 chunk_parser = nltk.RegexpParser(grammar)
+
+
 tree = chunk_parser.parse(res)
-tree.draw()
+#print(type(tree))
+
+
+
+def chunking(tweet):
+    res = pos_tagger.tag(tweet)
+    tree = chunk_parser.parse(res)
+    list = extract_chunks(tree)
+    return list
+
+#Transforme l'arbre en liste de branches
+def extract_chunks(tree):
+    chunks = []
+    for i in range(len(tree)):
+        chunks_inter = []
+        for j in range (len(tree[i])):
+            chunks_inter.append(tree[i][j])
+        chunks.append(chunks_inter)
+    return chunks
+            
